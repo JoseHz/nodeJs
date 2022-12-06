d26d0cef3a4b31fd0714f198cc83696330584532
Jose Pineda <jose.pineda@ae.edu.pe>
2022-12-06 16:53:49 -0500

    avanzando

--- api-rest-expressjs/src/index.js
+++ api-rest-expressjs/src/index.js
@@ -1,50 +1,26 @@
+const { urlencoded } = require('express');
 const express = require('express');
+const path = require('path');
+
+
+
+//inicializations
 const app = express();
-const { faker } = require('@faker-js/faker') ;
+
 
 //settings
 app.set('port', process.env.PORT || 3000);
 
+
+//middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
 //routes
-app.get("/", (req,res) => {
-  res.send('hello world')
-})
+app.use('/api/v1', require('./routes'))
 
-app.get("/products", (req, res) => {
-  const products = [];
-  for (let index = 0; index < 100; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    })
 
-    res.json(products)
-  }
 
-})
-
-app.get("/products/:id", (req, res) => {
-  const { id } = req.params;
-  res.send({
-      id,
-      name: 'lechuga',
-      price: 5
-    })
-})
-
-app.get('/users', (req,res) => {
-  const {limit, offset} = req.query;
-
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset
-    })
-  } else {
-    res.send('No hay limits and params')
-  }
-})
 
 //start server
 app.listen(app.get('port'), () => {
--- api-rest-expressjs/src/routes/index.js
+++ api-rest-expressjs/src/routes/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/products', require('./products.router'));
+router.use('/users', require('./users.route'));
+router.get("/", (req,res) => {
+  res.send('hello world')
+})
+
 module.exports = router;
